c2c151a1ccb80053d0854173499c8775eeafb8ad
a.karpov <a.karpov@skbkontur.ru>
2024-09-20 15:47:01 +0500

    init refactoring

--- App.axaml.cs
+++ App.axaml.cs
@@ -4,7 +4,7 @@ using Avalonia.Markup.Xaml;
 
 namespace IoTDeviceSimulation;
 
-public partial class App : Application
+public class App : Application
 {
     public override void Initialize()
     {
